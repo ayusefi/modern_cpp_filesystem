6ba4d29161cf75154d8e00968d028d85c7a831ac
Abdullah Yusefi <a.yusefi1991@gmail.com>
2021-12-29 15:11:26 +0300

    first commit

---
+++ homework_4/build/CMakeFiles/homework_4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_4.dir/homework_4.cpp.o"
+  "libhomework_4.pdb"
+  "libhomework_4.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
